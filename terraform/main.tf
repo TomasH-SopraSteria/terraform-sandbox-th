9b1b1d7fc9f7aed1fd2c14d3d32a94dbbd584173
theide <tomas.heide@soprasteria.com>
2022-11-03 15:53:20 +0100

    committest

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
-# Create a resource group
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraformdevops"
+    storage_account_name = "tfstorageaccthsandbox"
+    container_name       = "terraformstatefile"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "rg_sandbox-th" {
     name = var.rg_name
     location = var.location
