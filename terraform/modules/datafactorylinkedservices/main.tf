ebe0a904ae90b39722c657d41f7b9b607b23da8f
theide <tomas.heide@soprasteria.com>
2022-11-08 17:21:29 +0100

    balad

--- terraform/modules/datafactorylinkedservices/main.tf
+++ terraform/modules/datafactorylinkedservices/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_data_factory_linked_service_key_vault" "linkedKV" {
   name            = "linkedKV"
   data_factory_name = var.dfl_datafactory_name
   resource_group_name = var.dfl_resource_group_name
-  data_factory_id = var.dfl_keyvaultid
-  key_vault_id    = var.dfl_datafactoryid
+  #data_factory_id = var.dfl_datafactoryid
+  key_vault_id    = var.dfl_keyvaultid
 }
