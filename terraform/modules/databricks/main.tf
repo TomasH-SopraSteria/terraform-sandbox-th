683e56bbb447fc89086687697dfcaf7b7eb0db70
theide <tomas.heide@soprasteria.com>
2022-11-10 09:10:39 +0100

    adddatabricks

--- terraform/modules/databricks/main.tf
+++ terraform/modules/databricks/main.tf
@@ -1,8 +1,21 @@
-resource "azurerm_databricks_workspace" "example" {
+resource "azurerm_databricks_workspace" "databricks" {
   name                = var.db_name
+  resource_group_name = var.db_resource_group_name
+  location            = var.db_location
+  sku                 = var.db_sku
+
+  tags = {
+    Environment = var.db_tag_environment
+  }
+}
+
+resource "azurerm_databricks_access_connector" "example" {
+  name                = "databrickstest"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  sku                 = "standard"
+  identity {
+    type = "SystemAssigned"
+  }
 
   tags = {
     Environment = "Production"
---
+++ terraform/modules/databricks/variables.tf
@@ -0,0 +1,25 @@
+variable db_name {
+    type = string
+    description = "DF name"
+}
+
+variable db_location {
+    type = string
+}
+
+variable db_resource_group_name {
+    type = string
+    description = "Resource group name"
+}
+
+variable db_location {
+    type = string
+}
+
+variable db_sku {
+    type = string
+}
+
+variable db_tag_environment {
+    type = string
+}
